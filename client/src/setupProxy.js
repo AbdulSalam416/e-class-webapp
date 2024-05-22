289da0f451d4b1136d5ea0db632c4cba7964c0cf
Abdul Salam <60816007+AbdulSalam416@users.noreply.github.com>
2024-05-22 19:03:15 +0000

    Update setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,8 +1,9 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const proxyTarget = process.env.REACT_APP_PROXY_URL || 'https://e-class-webapp.onrender.com/';
 
 module.exports = function (app) {
   app.use(
     "/api",
-    createProxyMiddleware({ target:process.env.REACT_APP_PROXY_URL, changeOrigin: true })
+    createProxyMiddleware({ target:proxyTarget, changeOrigin: true })
   );
 };
